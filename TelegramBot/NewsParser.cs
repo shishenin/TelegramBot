29a2ed3890ac326cbaf6f1d65e64d086039c5ca0
eagle eagle <nv.shishenin@gmail.com>
2021-06-17 19:14:04 +0300

    Initial commit

---
+++ TelegramBot/NewsParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+
+namespace TelegramBot
+{
+    public static class NewsParser
+    {
+        public static async Task<string> FindNews(string text)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var document = await context.OpenAsync($"https://dtf.ru/search/v2/content/new?query={text}");
+            var elements = document.QuerySelectorAll("div.content-title.content-title--short.l-island-a");
+            var arr = elements.Select(it => it.TextContent).Take(5);
+            
+            return string.Join('\n', arr);
+        }
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot
+{
+    internal static class Program 
+    {
+        static ITelegramBotClient bot;
+
+        private static void Main()
+        {
+            bot = new TelegramBotClient("1892245518:AAEJWf5NPWkyLbItD-umVI0eO0a6Z4ZlByk");
+            bot.OnMessage += FirstOnMessage;
+            
+            bot.StartReceiving();
+            Console.ReadLine();
+            bot.StopReceiving();
+        }
+
+        static async void FirstOnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text == null) return;
+            await bot.SendTextMessageAsync(chatId: e.Message.Chat, text: await NewsParser.FindNews(e.Message.Text));
+        }
+    }
+}
